9d5b21e48108f26eb45caad62a6865ec23c9087b
Jordan Dawson <jtdawson@icloud.com>
2022-12-28 22:05:03 +0000

    completed creating dummy data and verifying that they both work on desktop and mobile.

--- js/data.js
+++ js/data.js
@@ -1,4 +1,8 @@
 /* exported data */
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('journal-local-storage', dataJSON);
+});
 
 var data = {
   view: 'entry-form',
@@ -6,3 +10,7 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var previousDataJSON = localStorage.getItem('journal-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
